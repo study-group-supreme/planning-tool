2bd70d42e5492afdb4afffdcc1eb184ccf52ce75
svanholm200-debug <svanholm200@gmail.com>
2026-05-04 08:44:59 +0200

    Initial commit

---
+++ src/main/java/PlanningToolApplication.java
@@ -0,0 +1,11 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanningToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlanningToolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/planningtool/PlanningToolApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.planningtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanningToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
