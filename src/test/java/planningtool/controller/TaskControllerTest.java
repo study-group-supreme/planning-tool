d71a603fe2475701445d21b51faa5bef077434dd
ellidena <85340254+ellidena@users.noreply.github.com>
2026-05-06 11:11:46 +0200

    Add annotations and injections TaskControllerTest

--- src/test/java/planningtool/controller/TaskControllerTest.java
+++ src/test/java/planningtool/controller/TaskControllerTest.java
@@ -1,4 +1,12 @@
 package planningtool.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(TaskController.class)
 public class TaskControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
 }
