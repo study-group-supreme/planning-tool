1c252150b466267d55aa5efbb328680e9ac410e3
Andreas Lysholm Jensen <goegl12@gmail.com>
2026-05-06 11:48:46 +0200

    Add Rowmapper, jdbc, and findEmployeeByEmail to EmployeeRepository

--- src/main/java/planningtool/repository/EmployeeRepository.java
+++ src/main/java/planningtool/repository/EmployeeRepository.java
@@ -1,4 +1,33 @@
 package planningtool.repository;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+import planningtool.model.Employee;
+
+@Repository
 public class EmployeeRepository {
+    private final JdbcTemplate jdbc;
+    private final RowMapper<Employee> employeeRowMapper = (rs, rowNum) -> {
+        Employee employee = new Employee();
+        employee.setId(rs.getInt("id"));
+        employee.setName(rs.getString("name"));
+        employee.setEmail(rs.getString("email"));
+        employee.setPassword("password");
+        employee.setProjectManager(rs.getBoolean("is_project_manager"));
+        return employee;
+    };
+
+    public EmployeeRepository(JdbcTemplate jdbc){
+        this.jdbc = jdbc;
+    }
+
+    public RowMapper<Employee> getEmployeeRowMapper() {
+        return employeeRowMapper;
+    }
+
+    public Employee findEmployeeByEmail(String email){
+        String sql = "SELECT * FROM employee WHERE email = ?";
+        return jdbc.queryForObject(sql, employeeRowMapper, email);
+    }
 }
