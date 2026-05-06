b9126d3bc57ffee5f5b1a542637d0193c84aeccb
svanholm200-debug <svanholm200@gmail.com>
2026-05-06 10:16:47 +0200

    Filled class with fields from class diagram, and added constuctors, getters and setters

--- src/main/java/model/TimeEntry.java
+++ src/main/java/model/TimeEntry.java
@@ -1,5 +1,63 @@
 package model;
 
-public class TimeEntry {
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
+public class TimeEntry {
+int id;
+int employeeId;
+int taskId;
+LocalDateTime logginTime;
+BigDecimal timeSpent;
+
+    public TimeEntry(int id, int employeeId, int taskId, LocalDateTime logginTime, BigDecimal timeSpent) {
+        this.id = id;
+        this.employeeId = employeeId;
+        this.taskId = taskId;
+        this.logginTime = logginTime;
+        this.timeSpent = timeSpent;
+    }
+    public TimeEntry(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public int getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(int taskId) {
+        this.taskId = taskId;
+    }
+
+    public LocalDateTime getLogginTime() {
+        return logginTime;
+    }
+
+    public void setLogginTime(LocalDateTime logginTime) {
+        this.logginTime = logginTime;
+    }
+
+    public BigDecimal getTimeSpent() {
+        return timeSpent;
+    }
+
+    public void setTimeSpent(BigDecimal timeSpent) {
+        this.timeSpent = timeSpent;
+    }
 }
