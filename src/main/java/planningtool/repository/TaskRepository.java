84bb27088b7783f640b2d7b50407e8e1b6069f00
August Skipper <funkskipper@hotmail.com>
2026-05-06 13:57:18 +0200

    Rearranged the getter method for the RowMapper in TaskRepository so that it is on top - also wrote a test method for findTaskById and confirmed that it passes

--- src/main/java/planningtool/repository/TaskRepository.java
+++ src/main/java/planningtool/repository/TaskRepository.java
@@ -9,11 +9,11 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class TaskRepository {
     private final JdbcTemplate jdbc;
-    private final RowMapper<Task> taskRowMapper = (rs, rowNum ) -> {
+    private final RowMapper<Task> taskRowMapper = (rs, rowNum) -> {
         Task task = new Task();
         task.setId(rs.getInt("id"));
         task.setProjectId(rs.getInt("project_id"));
-        task.setParentTaskId(rs.getInt("parent_task_id"));
+        task.setParentTaskId((Integer) rs.getObject("parent_task_id",Integer.class));
         task.setTitle(rs.getString("title"));
         task.setDescription(rs.getString("description"));
         task.setTimeEstimate(rs.getBigDecimal("time_estimate"));
@@ -21,19 +21,21 @@ public class TaskRepository {
         return task;
     };
 
-    public TaskRepository (JdbcTemplate jdbc) {
+    public TaskRepository(JdbcTemplate jdbc) {
         this.jdbc = jdbc;
-    }
-
-    public Task findTaskById(int taskId) {
-        String sql = """
-                        SELECT task.id, task.project_id, task.parent_task_id, task.title, task.description, task.time_estimate, task.is_high_priority
-                        FROM task
-                        WHERE id = ?""";
-        return jdbc.queryForObject(sql, taskRowMapper,taskId);
     }
 
     public RowMapper<Task> getTaskRowMapper() {
         return taskRowMapper;
     }
+
+    public Task findTaskById(int taskId) {
+        String sql = """
+                SELECT *
+                FROM task
+                WHERE id = ?""";
+        return jdbc.queryForObject(sql, taskRowMapper, taskId);
+    }
+
+
 }
