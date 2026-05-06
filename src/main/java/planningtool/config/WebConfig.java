54dfaf2b961d894cbe5113a0434eaa12bd7df16f
ellidena <85340254+ellidena@users.noreply.github.com>
2026-05-06 14:02:36 +0200

    Add initial webConfig

--- src/main/java/planningtool/config/WebConfig.java
+++ src/main/java/planningtool/config/WebConfig.java
@@ -1,4 +1,15 @@
 package planningtool.config;
 
-public class WebConfig {
+
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import planningtool.interceptor.LoginInterceptor;
+
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor())
+                .excludePathPatterns("/**");
+    }
 }
