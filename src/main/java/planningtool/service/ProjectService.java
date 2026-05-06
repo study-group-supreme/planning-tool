8b357ee82f920327b2bf84918e61292f02332998
svanholm200-debug <svanholm200@gmail.com>
2026-05-06 13:54:56 +0200

    Added service methods for projectService

--- src/main/java/planningtool/service/ProjectService.java
+++ src/main/java/planningtool/service/ProjectService.java
@@ -1,7 +1,30 @@
 package planningtool.service;
 
 import org.springframework.stereotype.Service;
+import planningtool.model.Employee;
+import planningtool.model.Project;
+import planningtool.model.Task;
+import planningtool.repository.ProjectRepository;
+
+import java.util.List;
 
 @Service
 public class ProjectService {
+    private final ProjectRepository projectRepository;
+
+    public ProjectService(ProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
+
+    public Project createProject(Project project) {
+        return projectRepository.insertProject(project);
+    }
+
+    public List<Task> getTaskByProjectId(int id) {
+        return projectRepository.findTasksByProjectId(id);
+    }
+
+    public List<Employee> getProjectMembersByProjectId(int id) {
+        return projectRepository.findProjectMembersByProjectId(id);
+    }
 }
