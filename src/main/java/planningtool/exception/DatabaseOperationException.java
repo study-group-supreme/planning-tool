27ac33806e6c73f60ca096c70c1af26907eabf09
August Skipper <funkskipper@hotmail.com>
2026-05-06 16:47:32 +0200

    Formatted TaskRepository for neatness, created TaskService constructor repository field, called the getTaskById() and createTask() methods from TaskRepository, and wrote the DatabaseOperationException custom exception we need to catch errors for it!

---
+++ src/main/java/planningtool/exception/DatabaseOperationException.java
@@ -0,0 +1,7 @@
+package planningtool.exception;
+
+public class DatabaseOperationException extends RuntimeException {
+    public DatabaseOperationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
