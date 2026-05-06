9630546e2b04f723f9daf90d8cfa33265274be54
Andreas Lysholm Jensen <goegl12@gmail.com>
2026-05-06 15:35:01 +0200

    add login method EmployeeService and login test for wrong email

--- src/main/java/planningtool/service/EmployeeService.java
+++ src/main/java/planningtool/service/EmployeeService.java
@@ -1,11 +1,14 @@
 package planningtool.service;
 
+import org.springframework.dao.DataAccessException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
 import planningtool.exception.BadRequestException;
 import planningtool.exception.NotFoundException;
 import planningtool.model.Employee;
 import planningtool.repository.EmployeeRepository;
 
+@Service
 public class EmployeeService {
     private EmployeeRepository employeeRepository;
 
@@ -14,14 +17,15 @@ public class EmployeeService {
     }
 
     public Employee login(String email, String password){
+        Employee employee;
         try{
-            Employee employeeLoginIn = employeeRepository.findEmployeeByEmail(email);
-            if(!employeeLoginIn.getPassword().equals(password)){
-                throw new BadRequestException("Incorrect password");
-            }
-            return employeeLoginIn;
-        } catch (EmptyResultDataAccessException e){
-            throw new NotFoundException("User not found");
+            employee = employeeRepository.findEmployeeByEmail(email);
+        } catch(EmptyResultDataAccessException e){
+            throw new NotFoundException("Email is not in the system");
         }
+        if (!employee.getPassword().equals(password)){
+            throw new BadRequestException("Incorrect password");
+        }
+        return employee;
     }
 }
