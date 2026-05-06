7eb1e915ca47495987aeed12a46c0e58d4cadbe5
ellidena <85340254+ellidena@users.noreply.github.com>
2026-05-06 11:23:52 +0200

    Add annotation to TaskRespositoryTest

--- src/test/java/planningtool/integration/TaskRepositoryTest.java
+++ src/test/java/planningtool/integration/TaskRepositoryTest.java
@@ -1,4 +1,15 @@
 package planningtool.integration;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import planningtool.repository.TaskRepository;
+
+@SpringBootTest
+@Sql(scripts = "classpath:h2init.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 public class TaskRepositoryTest {
+
+    // TODO: add this back in when taskRepository actually contains something
+//    @Autowired
+//    private TaskRepository taskRepository;
 }
