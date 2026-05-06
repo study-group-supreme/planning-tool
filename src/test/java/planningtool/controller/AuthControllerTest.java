979bc36835b5b4198e78439daac5318d9a6fe3e6
ellidena <85340254+ellidena@users.noreply.github.com>
2026-05-06 11:09:18 +0200

    Add annotations and injections AuthControllerTest

--- src/test/java/planningtool/controller/AuthControllerTest.java
+++ src/test/java/planningtool/controller/AuthControllerTest.java
@@ -1,4 +1,19 @@
 package planningtool.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import planningtool.service.EmployeeService;
+
+@WebMvcTest(AuthController.class)
 public class AuthControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private EmployeeService employeeService;
+
+
 }
