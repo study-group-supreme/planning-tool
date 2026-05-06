a65e5b509c1d4e249b7c15b5bf79f099caf35850
ellidena <85340254+ellidena@users.noreply.github.com>
2026-05-06 11:10:38 +0200

    Add annotations and injections ProjectControllerTest

--- src/test/java/planningtool/controller/ProjectControllerTest.java
+++ src/test/java/planningtool/controller/ProjectControllerTest.java
@@ -1,4 +1,12 @@
 package planningtool.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(ProjectController.class)
 public class ProjectControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
 }
