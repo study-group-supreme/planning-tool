15e274da578c7db383c098dfe3bb3639eeac0402
ellidena <85340254+ellidena@users.noreply.github.com>
2026-05-06 11:07:19 +0200

    Add annotation and injections EmployeeServiceTest

--- src/test/java/planningtool/service/EmployeeServiceTest.java
+++ src/test/java/planningtool/service/EmployeeServiceTest.java
@@ -1,4 +1,17 @@
 package planningtool.service;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import planningtool.repository.EmployeeRepository;
+
+@ExtendWith(MockitoExtension.class)
 public class EmployeeServiceTest {
+
+    @Mock
+    private EmployeeRepository employeeRepository;
+
+    @InjectMocks
+    private EmployeeService employeeService;
 }
