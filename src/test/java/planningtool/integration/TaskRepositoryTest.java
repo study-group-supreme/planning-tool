e388b8c9cbadfc4d7fd34bc63099ed3660ad98aa
August Skipper <funkskipper@hotmail.com>
2026-05-06 14:14:50 +0200

    Fleshed out the test so it asserts ALL of the fields in findTaskById method

--- src/test/java/planningtool/integration/TaskRepositoryTest.java
+++ src/test/java/planningtool/integration/TaskRepositoryTest.java
@@ -23,5 +23,11 @@ private TaskRepository taskRepository;
     Task task = taskRepository.findTaskById(1);
 
     assertThat(task.getTitle()).isEqualTo("Brew coffee");
+    assertThat(task.isHighPriority()).isEqualTo(true);
+    assertThat(task.getTimeEstimate()).isEqualByComparingTo("0.5");
+    assertThat(task.getProjectId()).isEqualTo(1);
+    assertThat(task.getParentTaskId()).isNull();
+    assertThat(task.getDescription()).isNull();;
+
 }
 }
