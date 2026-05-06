e7b5f8d13a34b3912000a010f484a9556f9f67c0
svanholm200-debug <svanholm200@gmail.com>
2026-05-06 15:49:14 +0200

    imports

--- src/main/java/planningtool/service/ProjectService.java
+++ src/main/java/planningtool/service/ProjectService.java
@@ -1,6 +1,7 @@
 package planningtool.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import planningtool.exception.BadRequestException;
 import planningtool.model.Employee;
 import planningtool.model.Project;
