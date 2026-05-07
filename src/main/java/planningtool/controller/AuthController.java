a3c056a97c7a5863121a47939d15c6d8e43edc23
ellidena <85340254+ellidena@users.noreply.github.com>
2026-05-07 09:44:21 +0200

    Setup AuthController requestmapping and service injection

--- src/main/java/planningtool/controller/AuthController.java
+++ src/main/java/planningtool/controller/AuthController.java
@@ -1,7 +1,18 @@
 package planningtool.controller;
 
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import planningtool.service.EmployeeService;
 
 @Controller
+@RequestMapping("/auth")
 public class AuthController {
+    private final EmployeeService employeeService;
+
+    public AuthController(EmployeeService employeeService){
+        this.employeeService = employeeService;
+    }
+
+
 }
