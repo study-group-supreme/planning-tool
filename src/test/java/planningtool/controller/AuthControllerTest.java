9d099a915c51a66d6238f014ca1d6e62078f74b9
ellidena <85340254+ellidena@users.noreply.github.com>
2026-05-07 10:26:56 +0200

    Add Mvc test showLoginForm

--- src/test/java/planningtool/controller/AuthControllerTest.java
+++ src/test/java/planningtool/controller/AuthControllerTest.java
@@ -1,10 +1,15 @@
 package planningtool.controller;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import planningtool.service.EmployeeService;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(AuthController.class)
 public class AuthControllerTest {
@@ -15,5 +20,11 @@ public class AuthControllerTest {
     @MockitoBean
     private EmployeeService employeeService;
 
+    @Test
+    void showLoginForm_shouldDisplayLoginForm() throws Exception{
+        mockMvc.perform(get("/auth/login")) // this refers to the controller endpoint
+                .andExpect(status().isOk())
+                .andExpect(view().name("auth/login")); // This is the html template
+    }
 
 }
