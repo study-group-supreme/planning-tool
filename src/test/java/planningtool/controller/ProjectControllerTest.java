6a1c411af26e8936d9a9f80806de59f13e0ecde3
svanholm200-debug <svanholm200@gmail.com>
2026-05-07 09:58:41 +0200

    Added mocks to all service classes, and added a test for the get mapping of createProject

--- src/test/java/planningtool/controller/ProjectControllerTest.java
+++ src/test/java/planningtool/controller/ProjectControllerTest.java
@@ -1,15 +1,38 @@
 package planningtool.controller;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import planningtool.service.EmployeeService;
+import planningtool.service.ProjectService;
+import planningtool.service.TaskService;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(ProjectController.class)
 public class ProjectControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+    @MockitoBean
+    ProjectService projectService;
+    @MockitoBean
+    TaskService taskService;
+    @MockitoBean
+    EmployeeService employeeService;
 
+@Test
+    void createProject_shouldShowCreateProjectForm() throws Exception{
+    mockMvc.perform(get("/projects/add").sessionAttr("employeeId", 1))
+            .andExpect(status().isOk()).
+            andExpect(view().name("project/create-project"))
+            .andExpect(model().attributeExists("project"));
 
+}
 
 }
