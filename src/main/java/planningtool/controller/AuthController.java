ade310cf83a56da83368ddbcef8e33e496a211cf
ellidena <85340254+ellidena@users.noreply.github.com>
2026-05-07 10:09:32 +0200

    Add loginForm, loginFormHandler, logout

--- src/main/java/planningtool/controller/AuthController.java
+++ src/main/java/planningtool/controller/AuthController.java
@@ -1,9 +1,16 @@
 package planningtool.controller;
 
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import planningtool.exception.BadRequestException;
+import planningtool.exception.NotFoundException;
+import planningtool.model.Employee;
 import planningtool.service.EmployeeService;
 
 @Controller
@@ -15,6 +22,36 @@ public class AuthController {
         this.employeeService = employeeService;
     }
 
+    // change from wishlist is adding model and "error" attribute for html
     @GetMapping("/login")
-    public String showLoginForm(){return "auth/login";}
+    public String showLoginForm(Model model){
+        model.addAttribute("error", false);
+        return "auth/login";}
+
+    @PostMapping("login")
+    public String loginFormHandler(
+            @RequestParam("email") String email,
+            @RequestParam("password") String password,
+            HttpSession session,
+            Model model){
+
+
+        try{
+            Employee employee = employeeService.login(email, password);
+            session.setAttribute("employeeId", employee.getId());
+            session.setAttribute("employeeName", employee.getName());
+            return "redirect:/project/list"; // TODO just make it be the project overview when we have it
+        } catch (NotFoundException | BadRequestException e) {
+            model.addAttribute("error", true);
+            model.addAttribute("message", e.getMessage());
+            return "auth/login";
+        }
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session){
+        session.invalidate();
+        return "redirect:/auth/login";
+    }
+
 }
