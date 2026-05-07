502a98cc1ff58ee4dfc83f12e8fd37c03752f4f9
August Skipper <funkskipper@hotmail.com>
2026-05-07 11:59:07 +0200

    Wrote a createTask_returnsCreatedTask() method in the TaskServiceTest class

--- src/test/java/planningtool/service/TaskServiceTest.java
+++ src/test/java/planningtool/service/TaskServiceTest.java
@@ -8,11 +8,12 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import planningtool.exception.BadRequestException;
 import planningtool.model.Task;
 import planningtool.repository.TaskRepository;
-
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 import java.math.BigDecimal;
-import java.math.BigInteger;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+
 
 @ExtendWith(MockitoExtension.class)
 public class TaskServiceTest {
@@ -30,24 +31,24 @@ public class TaskServiceTest {
 
     @Test
     void createTask_returnsCreatedTask() {
-        Task testTask = new Task();
-        testTask.setId(3);
-        testTask.setProjectId(2);
-        testTask.setParentTaskId(null);
-        testTask.setTitle("Sweep floors");
-        testTask.setDescription("Lunch room");
-        testTask.setHighPriority(false);
-        testTask.setTimeEstimate(new BigDecimal("0.25"));
+        Task task = new Task();
+        task.setId(3);
+        task.setProjectId(2);
+        task.setParentTaskId(null);
+        task.setTitle("Sweep floors");
+        task.setDescription("Lunch room");
+        task.setHighPriority(false);
+        task.setTimeEstimate(new BigDecimal("0.25"));
 
-
+        when (taskRepository.insertTask(task)).thenReturn(task);
+        Task createdTask = taskService.createTask(task);
+        assertEquals(3,createdTask.getId());
+        assertEquals(2, createdTask.getProjectId());
+        assertNull(createdTask.getParentTaskId());
+        assertEquals("Sweep floors", createdTask.getTitle());
+        assertEquals("Lunch room", createdTask.getDescription());
+        assertFalse(createdTask.isHighPriority());
+        assertThat(createdTask.getTimeEstimate()).isEqualByComparingTo("0.25");
     }
-
-
-
-
-
-
-
-
 
 }
