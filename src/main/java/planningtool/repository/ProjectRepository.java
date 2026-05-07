6429b5930ca5011f651228c4687527a83cff79ee
svanholm200-debug <svanholm200@gmail.com>
2026-05-07 11:51:36 +0200

    Added findProjectByEmployeeId method

--- src/main/java/planningtool/repository/ProjectRepository.java
+++ src/main/java/planningtool/repository/ProjectRepository.java
@@ -97,5 +97,15 @@ public class ProjectRepository {
         String sql = "SELECT * FROM project WHERE id = ?";
         return jdbc.queryForObject(sql,projectRowMapper, id);
     }
+    public List<Project> findProjectByEmployeeId(int id){
+        String sql = """
+                SELECT project.id, project.title, project.description, project.time_of_creation, project.project_manager_id, project.deadline, project.active
+                FROM project
+                JOIN project_member
+                ON project_member.project_id = project.id
+                WHERE project_member.employee_id = ? 
+                """;
+        return jdbc.query(sql, projectRowMapper, id);
+    }
 
 }
