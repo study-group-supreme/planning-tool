c007be4a7f28d6d23e83150a61309a9373f46ce4
svanholm200-debug <svanholm200@gmail.com>
2026-05-08 09:32:06 +0200

    added EmployeeRepo to projectservice to display clearer error message

--- src/main/java/planningtool/service/ProjectService.java
+++ src/main/java/planningtool/service/ProjectService.java
@@ -16,9 +16,11 @@ import java.util.List;
 @Service
 public class ProjectService {
     private final ProjectRepository projectRepository;
+    private final EmployeeRepository employeeRepository;
 
-    public ProjectService(ProjectRepository projectRepository) {
+    public ProjectService(ProjectRepository projectRepository, EmployeeRepository employeeRepository) {
         this.projectRepository = projectRepository;
+        this.employeeRepository=employeeRepository;
     }
 
     @Transactional
@@ -53,11 +55,12 @@ public class ProjectService {
         }
         return members;
     }
-    public List<Project> getProjectsByEmployeeId(int employeeId){
+    public List<Project> getProjectsByEmployeeId(int employeeId, Employee employee){
         List<Project> projects = projectRepository.findProjectsByEmployeeId(employeeId);
         if (projects == null || projects.isEmpty()){
             //Need help with good error message
-            throw new NotFoundException("No projects found connected to");
+            throw new NotFoundException("No projects found connected to " + employee.getName());
+
         }
         return projects;
     }
