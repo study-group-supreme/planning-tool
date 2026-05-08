00f4f5ce9fcdd5ca8a70db3ff0b48169618a868e
ellidena <85340254+ellidena@users.noreply.github.com>
2026-05-08 12:58:24 +0200

    Edit TaskRepositoryTest.insertTask to include isDone

--- src/test/java/planningtool/integration/TaskRepositoryTest.java
+++ src/test/java/planningtool/integration/TaskRepositoryTest.java
@@ -46,6 +46,7 @@ public class TaskRepositoryTest {
         newTask.setDescription("Extra focused on no corners poking out whatsoever");
         newTask.setTimeEstimate(new BigDecimal("0.25"));
         newTask.setHighPriority(true);
+        newTask.setDone(false);
 
         Task result = taskRepository.insertTask(newTask);
 
@@ -56,5 +57,6 @@ public class TaskRepositoryTest {
         assertThat(result.getDescription()).isEqualTo("Extra focused on no corners poking out whatsoever");
         assertThat(result.getTimeEstimate()).isEqualByComparingTo("0.25");
         assertThat(result.isHighPriority()).isEqualTo(true);
+        assertThat(result.isDone()).isFalse();
     }
 }
