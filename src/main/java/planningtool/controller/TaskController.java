8b04e66e7d0e59e63b2b7f3bc5a2d8cc281ca87a
ellidena <85340254+ellidena@users.noreply.github.com>
2026-05-10 14:57:12 +0200

    Add TaskController annotations and service injections

--- src/main/java/planningtool/controller/TaskController.java
+++ src/main/java/planningtool/controller/TaskController.java
@@ -1,4 +1,18 @@
 package planningtool.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import planningtool.service.TaskService;
+
+@Controller
+@RequestMapping("/tasks")
 public class TaskController {
+
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+
 }
