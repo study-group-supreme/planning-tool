c554f3e45a06b9b8d2bb51000cf297a3a0926bd3
ellidena <85340254+ellidena@users.noreply.github.com>
2026-05-10 10:45:52 +0200

    Add initial TaskService.createTimeEntry method

--- src/main/java/planningtool/service/TaskService.java
+++ src/main/java/planningtool/service/TaskService.java
@@ -9,9 +9,11 @@ import planningtool.exception.BadRequestException;
 import planningtool.exception.DatabaseOperationException;
 import planningtool.exception.NotFoundException;
 import planningtool.model.Task;
+import planningtool.model.TimeEntry;
 import planningtool.repository.TaskRepository;
 
 import javax.xml.crypto.Data;
+import java.math.BigDecimal;
 
 @Service
 public class TaskService {
@@ -44,5 +46,36 @@ public class TaskService {
         }
     }
 
+    @Transactional
+    public TimeEntry createTimeEntry(TimeEntry timeEntry) {
+        if (timeEntry == null) {
+            throw new BadRequestException("Time entry cannot be null");
+        }
+        if (timeEntry.getTaskId() <= 0) {
+            throw new BadRequestException("Invalid task id");
+        }
+
+
+        BigDecimal timeSpent = timeEntry.getTimeSpent();
+        if (timeSpent == null || timeSpent.compareTo(BigDecimal.ZERO) <=0) {
+            throw new BadRequestException("Time spent must be a positive number");
+        }
+
+        // TODO: Optionally, task-exists-check like try/catch block on line 31 above + a similar check for employee-exists additionally (employeeRepo would need to be imported tho)
+//        try {
+//            taskRepository.findTaskById(timeEntry.getTaskId());
+//        } catch (EmptyResultDataAccessException e) {
+//            throw new NotFoundException("Nothing to show for task with id:" + timeEntry.getTaskId());
+//        } catch (DataAccessException e) {
+//            throw new DatabaseOperationException("Database error while loading task", e);
+//        }
+
+
+        try {
+            return taskRepository.insertTimeEntry(timeEntry);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseOperationException("Failed to create time entry", e);
+        }
+    }
 
 }
