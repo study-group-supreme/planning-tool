cf6fa8383a0345c6b030052ea641a5d32167633d
svanholm200-debug <svanholm200@gmail.com>
2026-05-12 10:49:59 +0200

    Addeds deleteTaskById()

--- src/main/java/planningtool/repository/TaskRepository.java
+++ src/main/java/planningtool/repository/TaskRepository.java
@@ -48,7 +48,9 @@ public class TaskRepository {
         return taskRowMapper;
     }
 
-    public RowMapper<TimeEntry> getTimeEntryRowMapper(){ return  timeEntryRowMapper; }
+    public RowMapper<TimeEntry> getTimeEntryRowMapper() {
+        return timeEntryRowMapper;
+    }
 
     public Task insertTask(Task task) {
         String sql = """
@@ -114,12 +116,19 @@ public class TaskRepository {
         jdbc.update(sql, id);
     }
 
-    public void updateTask (Task task){
+    public void updateTask(Task task) {
         String sql = """
                 UPDATE task
                 SET parent_task_id = ?, assigned_member_id = ?, title = ?, description = ?, time_estimate = ?, is_high_priority = ?, is_done = ?
                 """;
         jdbc.update(sql, task.getParentTaskId(), task.getAssignedMemberId(), task.getTitle(), task.getDescription(), task.getTimeEstimate(), task.isHighPriority(), task.isDone());
+    }
+
+    public void deleteTaskById(int id) {
+        String sql = """
+                DELETE FROM task WHERE id = ?
+                """;
+        jdbc.update(sql, id);
     }
 
 
