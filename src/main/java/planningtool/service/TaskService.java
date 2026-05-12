51543305a02dc24967fe3b74d66ef98ab3dd8b6c
svanholm200-debug <svanholm200@gmail.com>
2026-05-12 12:15:50 +0200

    Added to the removeTaskById method in task service, so that it checks for if its a main task and gets a list of subtask and thorws an exeption if all subtask isent done when trying to delete main task

--- src/main/java/planningtool/service/TaskService.java
+++ src/main/java/planningtool/service/TaskService.java
@@ -14,6 +14,7 @@ import planningtool.repository.TaskRepository;
 
 import javax.xml.crypto.Data;
 import java.math.BigDecimal;
+import java.util.List;
 
 @Service
 public class TaskService {
@@ -110,8 +111,18 @@ public class TaskService {
             throw new DatabaseOperationException(e.getMessage(), e.getCause());
         }
     }
-//Properly need some validation here at some point
+
+    //Properly need some validation here at some point
     public void removeTaskById(int id) {
+        Task task = taskRepository.findTaskById(id);
+        if (task.getParentTaskId() == null) {
+            List<Task> tasks = taskRepository.findSubtasksByParentId(id);
+            for (Task t : tasks) {
+                if (!t.isDone()) {
+                    throw new BadRequestException("...");
+                }
+            }
+        }
         taskRepository.deleteTaskById(id);
 
     }
