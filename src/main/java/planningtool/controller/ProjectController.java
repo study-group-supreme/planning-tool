f2e56583a7fa739ccf923ac7e61e64e3698ffec7
svanholm200-debug <svanholm200@gmail.com>
2026-05-13 11:03:50 +0200

    added getDoneTasks to projectcontroller

--- src/main/java/planningtool/controller/ProjectController.java
+++ src/main/java/planningtool/controller/ProjectController.java
@@ -32,7 +32,7 @@ public class ProjectController {
             List<Project> employeeProjects = projectService.getProjectsByEmployeeId(employeeId);
             model.addAttribute("projects", employeeProjects);
             return "project/list-projects";
-        } catch(NotFoundException e){
+        } catch (NotFoundException e) {
             model.addAttribute("emptyList", true);
             model.addAttribute("message", e.getMessage());
             return "project/list-projects";
@@ -56,8 +56,10 @@ public class ProjectController {
 
     @GetMapping("/{projectId}")
     public String showSpecificProject(@PathVariable int projectId, Model model) {
-        model.addAttribute("project", projectService.getProjectById(projectId));
+        Project project = projectService.getProjectById(projectId);
+        model.addAttribute("project", project);
         model.addAttribute("mainTask", false);
+        model.addAttribute("doneTasks", taskService.getDoneTasks(project.getTasks()));
         return "project/details-project";
     }
 
