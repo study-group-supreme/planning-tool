ee31809dc037e80c5db3eb8c10d99ab7a3d164bd
ellidena <85340254+ellidena@users.noreply.github.com>
2026-05-15 10:54:16 +0200

    Edit h2 and test to match

--- src/test/java/planningtool/integration/TaskRepositoryTest.java
+++ src/test/java/planningtool/integration/TaskRepositoryTest.java
@@ -37,7 +37,7 @@ public class TaskRepositoryTest {
         assertThat(task.getTitle()).isEqualTo("Brew coffee");
         assertThat(task.isHighPriority()).isEqualTo(true);
         assertThat(task.getAssignedMemberId()).isEqualTo(2);
-        assertThat(task.getTimeEstimate()).isEqualByComparingTo("0.5");
+        assertThat(task.getTimeEstimate()).isEqualByComparingTo("1.5");
         assertThat(task.getProjectId()).isEqualTo(1);
         assertThat(task.getParentTaskId()).isNull();
         assertThat(task.getDescription()).isNull();
