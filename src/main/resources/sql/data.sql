d8beea04adca61e46d70b07c8b9eafbe96ba946d
ellidena <85340254+ellidena@users.noreply.github.com>
2026-05-15 10:51:26 +0200

    Edit data.sql tasks to have min time estimate at minimum 1

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -25,9 +25,9 @@ VALUES (1, 1),
        (4, 1);
 
 INSERT INTO task (project_id, parent_task_id, assigned_member_id, title, description, time_estimate, is_high_priority, is_done)
-VALUES (1, null, 2, 'Brew coffee', null, '0.5', true, false),
-       (1, 1, 3, 'Grind the beans', null, '0.25', true, false),
-       (1, 1, 1, 'Pour boiling water', null, '0.25', true, false),
+VALUES (1, null, 2, 'Brew coffee', null, '1.5', true, false),
+       (1, 1, 3, 'Grind the beans', null, '1.0', true, false),
+       (1, 1, 1, 'Pour boiling water', null, '1.0', true, false),
        (2, null, 4, 'Motivational speech', null, '1', false,false);
 
 INSERT INTO time_entry (employee_id, task_id, time_spent)
