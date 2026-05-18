5a306e669697565adaae9f9b859f86576ffef432
svanholm200-debug <svanholm200@gmail.com>
2026-05-18 13:14:37 +0200

    Removed the object

--- src/main/java/planningtool/service/EmployeeService.java
+++ src/main/java/planningtool/service/EmployeeService.java
@@ -37,15 +37,11 @@ public class EmployeeService {
     }
 
     public Employee getEmployeeById(int id) {
-        Employee employee;
-
         try {
-            employee = employeeRepository.findEmployeeById(id);
+            return employeeRepository.findEmployeeById(id);
         } catch (EmptyResultDataAccessException e) {
             throw new NotFoundException("No employee found ");
 
         }
-        return employee;
-
     }
 }
