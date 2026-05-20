365725b370171df87795299621092b49e1dc4518
ellidena <85340254+ellidena@users.noreply.github.com>
2026-05-20 13:53:44 +0200

    Made sumTimeEntries public

--- src/main/java/planningtool/service/ProjectService.java
+++ src/main/java/planningtool/service/ProjectService.java
@@ -231,7 +231,7 @@ public class ProjectService {
         return total;
     }
 
-    private BigDecimal sumTimeEntriesForTask(int taskId){
+    public BigDecimal sumTimeEntriesForTask(int taskId){
         List<TimeEntry> entries = taskRepository.findTimeEntriesByTaskId(taskId);
         BigDecimal total = BigDecimal.ZERO;
         for (TimeEntry entry : entries){
