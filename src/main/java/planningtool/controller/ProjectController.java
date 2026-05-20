d66292cdb9bc6c4590d46ba5634ce1e522deee2d
svanholm200-debug <svanholm200@gmail.com>
2026-05-20 16:46:22 +0200

    Added isArchived to show specifikProject. and added th:unless to all create and edit buttons

--- src/main/java/planningtool/controller/ProjectController.java
+++ src/main/java/planningtool/controller/ProjectController.java
@@ -89,6 +89,7 @@ public class ProjectController {
         model.addAttribute("progressMap", taskService.getMainTaskProgress(projectId));
         model.addAttribute("currentUserId", currentUserId);
         model.addAttribute("totalEstimate", totalEstimate);
+        model.addAttribute("isArchived", !project.isActive());
         return "project/details-project";
     }
 
