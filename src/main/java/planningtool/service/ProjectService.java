eae32926586be8f89a6ac1943544faa966aebe74
Andreas Lysholm Jensen <goegl12@gmail.com>
2026-05-26 20:23:07 +0200

    fixed spelling error

--- src/main/java/planningtool/service/ProjectService.java
+++ src/main/java/planningtool/service/ProjectService.java
@@ -284,7 +284,7 @@ public class ProjectService {
         return projectRepository.findAllProjects();
     }
 
-    //TODO Should use findByMainTasksByProjectId() instead of checking for children
+    //TODO Should use findMainTasksByProjectId() instead of checking for children
     @Transactional
     public BigDecimal calculateEstimatedPriceForProject(int projectId) {
         BigDecimal estimatedCost = BigDecimal.ZERO;
